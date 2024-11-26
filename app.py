7807cbd912d6947912a0dc32ec60d1e13a49f314
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-11-26 11:35:19 +0100

    [feat] updated example for working with the loop

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from config import MESSAGE_FETCH_INTERVAL
 from sms_manager import SMSDataManager
 from sqlite_manager import SQLiteDataManger
 
+from external_api.jokes import get_joke_from_api
+
 # Create Database-Manager
 sqlite_manager = SQLiteDataManger()
 
@@ -11,7 +13,15 @@ sqlite_manager = SQLiteDataManger()
 sms_manager = SMSDataManager()
 
 # Receive messages in a timed-interval
+# Basic usage added
 while True:
     received_messages = sms_manager.get_messages()
     print(received_messages)
+
+    for [number, data] in received_messages.items:
+        message = data["text"]
+
+        if "joke" in message.lower():
+            sms_manager.send_sms(number, get_joke_from_api(), "DailyMoodBoost")
+
     time.sleep(MESSAGE_FETCH_INTERVAL)

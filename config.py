1df9bb143f23c66ab13d4bd12d821364f8b11f6e
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-11-26 11:07:08 +0100

    [feat] updated preliminary datamanager

---
+++ config.py
@@ -0,0 +1,2 @@
+MASTERSCHOOL_API = "http://hackathons.masterschool.com:3030"
+MASTERSCHOOL_API_TEAMNAME = "team_1_nov_2024"
--- sms_manager.py
+++ sms_manager.py
@@ -1,7 +1,6 @@
 import requests
 
-MASTERSCHOOL_API = "http://hackathons.masterschool.com:3030"
-MASTERSCHOOL_API_TEAMNAME = "team_1_nov_2024"
+from config import MASTERSCHOOL_API, MASTERSCHOOL_API_TEAMNAME
 
 
 class DataManager:
@@ -13,8 +12,19 @@ class DataManager:
 
     @staticmethod
     def register_phone_number_to_team(phone_number, team):
-        return requests.post(f'{MASTERSCHOOL_API}')
+        return requests.post(f'{MASTERSCHOOL_API}/team/registerNumber', json={
+            "phoneNumber": phone_number,
+            "teamName": team
+        }).json()
 
     @staticmethod
     def get_messages():
         return requests.get(f'{MASTERSCHOOL_API}/team/getMessages/{MASTERSCHOOL_API_TEAMNAME}').json()
+
+    @staticmethod
+    def send_sms(phone_number, message, sender):
+        return requests.post(f'{MASTERSCHOOL_API}/sms/send', json={
+            "phoneNumber": phone_number,
+            "message": message,
+            "sender": sender
+        }).json()

51a9a38c8c3791ea5860893e304c60710bfa04c6
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-11-26 11:13:51 +0100

    [feat] added unregister function

--- sms_manager.py
+++ sms_manager.py
@@ -18,8 +18,15 @@ class DataManager:
         }).json()
 
     @staticmethod
-    def get_messages():
-        return requests.get(f'{MASTERSCHOOL_API}/team/getMessages/{MASTERSCHOOL_API_TEAMNAME}').json()
+    def unregister_phone_number_from_team(phone_number, team):
+        return requests.post(f'{MASTERSCHOOL_API}/team/registerNumber', json={
+            "phoneNumber": phone_number,
+            "teamName": team
+        }).json()
+
+    @staticmethod
+    def get_messages(team_name=MASTERSCHOOL_API_TEAMNAME):
+        return requests.get(f'{MASTERSCHOOL_API}/team/getMessages/{team_name}').json()
 
     @staticmethod
     def send_sms(phone_number, message, sender):

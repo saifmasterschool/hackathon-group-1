44aa7510caf73eafe34774649bb58523d0d6904d
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-11-26 13:38:02 +0100

    [chore] added if __name__ file run routine

--- main.py
+++ main.py
@@ -10,37 +10,43 @@ sqlite_manager = SQLiteDataManger()
 # Create SMS-manager
 sms_manager = SMSDataManager()
 
-# Receive messages in a timed-interval
-# Basic usage example
-while True:
-    # Get the timestamp of the last message
-    last_message_timestamp = sqlite_manager.get_last_message_timestamp()
 
-    # Filter all messages that only new messages are shown.
-    received_messages: list[dict] = sms_manager.get_filtered_messages(last_message_timestamp)
+def start_message_loop():
+    # Receive messages in a timed-interval
+    # Basic usage example
+    while True:
+        # Get the timestamp of the last message
+        last_message_timestamp = sqlite_manager.get_last_message_timestamp()
 
-    print(received_messages)
+        # Filter all messages that only new messages are shown.
+        received_messages: list[dict] = sms_manager.get_filtered_messages(last_message_timestamp)
 
-    # loop through all messages
-    for message in received_messages:
-        try:
-            sender_number = int(message["sender"])
-        except ValueError:
-            print("Sender number is not convertable to number")
-            continue
+        print(received_messages)
 
-        # Check if message is already handled
-        if sqlite_manager.check_if_message_handled({**message, "sender": sender_number}):
-            continue
+        # loop through all messages
+        for message in received_messages:
+            try:
+                sender_number = int(message["sender"])
+            except ValueError:
+                print("Sender number is not convertable to number")
+                continue
 
-        # ------------ Add message handlers here ---------- #
-        # Handle the message based on the content
-        if "joke" in message["text"].lower():
-            sms_manager.send_sms(sender_number, get_joke_from_api(), "Daily Joke from DailyMoodBoost")
+            # Check if message is already handled
+            if sqlite_manager.check_if_message_handled({**message, "sender": sender_number}):
+                continue
 
-        # ------------------------------------------------ #
+            # ------------ Add message handlers here ---------- #
+            # Handle the message based on the content
+            if "joke" in message["text"].lower():
+                sms_manager.send_sms(sender_number, get_joke_from_api(), "Daily Joke from DailyMoodBoost")
 
-        # Add message sqlite to mark as handled
-        sqlite_manager.add_message_to_log({**received_messages, "sender": sender_number})
+            # ------------------------------------------------ #
 
-    time.sleep(MESSAGE_FETCH_INTERVAL)
+            # Add message sqlite to mark as handled
+            sqlite_manager.add_message_to_log({**received_messages, "sender": sender_number})
+
+        time.sleep(MESSAGE_FETCH_INTERVAL)
+
+
+if __name__ == "__main__":
+    start_message_loop()

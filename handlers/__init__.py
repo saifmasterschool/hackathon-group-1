dac2a4dc599b99bdb460d9696a2ca1045b5fa168
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-11-26 21:16:05 +0100

    [chore] improved import statements

---
+++ handlers/__init__.py
@@ -0,0 +1,2 @@
+from .join_channel import join_channel
+from .subscribe_team import subscribe_team
---
+++ schemas/__init__.py
@@ -0,0 +1,3 @@
+from .user_schema import User
+from .jokes_schema import Jokes
+from .message_schema import Message

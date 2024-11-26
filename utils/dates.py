14a6da7017bd115f80bfa85315ccf5a76f7b5fd8
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-11-26 21:16:27 +0100

    [feat] added reusable utility functions

--- utils/dates.py
+++ utils/dates.py
@@ -2,4 +2,10 @@ from datetime import datetime
 
 
 def convert_timestamp(timestamp_str):
+    """
+    Takes in a string representing a date and converts it into a python date object.
+    Has to be of format: "%Y-%m-%dT%H:%M:%S.%f%z"
+    :param timestamp_str: The string representing a date.
+    :return: A Python date object
+    """
     return datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M:%S.%f%z").replace(tzinfo=None)
--- utils/information.py
+++ utils/information.py
@@ -1,4 +1,11 @@
 def print_worked_on_messages(sms_manager, sqlite_manager):
+    """
+    Fetches all Messages saved in the Masterschool API Server and from the local database.
+    Compares how many are already handled.
+    :param sms_manager: The DataManager for SMS.
+    :param sqlite_manager: The DataManager for SQLite.
+    :return: Prints the state in the console.
+    """
     messages_in_api = sum(
         len(msgs)
         for message_dict in sms_manager.get_messages()

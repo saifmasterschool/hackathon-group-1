296644cb9751667342a10f02e6f05789d2c47d96
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-11-27 12:46:57 +0100

    [feat] Added default message if message is not recognised.

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,3 +2,4 @@ from .join_channel import join_channel
 from .subscribe_team import subscribe_team
 from .unsubscribe_team import unsubscribe_team
 from .status_response import status_response
+from .leave_channel import leave_channel

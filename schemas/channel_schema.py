ff70f3a3eb832823f0f52569071c218d13d03ed0
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-11-27 22:04:48 +0100

    [feat] Added __str__ and __repr__

--- schemas/channel_schema.py
+++ schemas/channel_schema.py
@@ -7,6 +7,12 @@ from database.extension import Base
 class Channel(Base):
     __tablename__ = 'channels'
 
+    def __repr__(self):
+        return f"<Channel(channel_id={self.channel_id}, channel_name='{self.channel_name}')>"
+
+    def __str__(self):
+        return f"Channel: {self.channel_name} (ID: {self.channel_id})"
+
     channel_id = Column(Integer, primary_key=True, autoincrement=True)
     channel_name = Column(String)
 

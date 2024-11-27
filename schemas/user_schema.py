22d7899d7367cc8b07669a903da70f4fc2b3e410
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-11-27 14:01:56 +0100

    [feat] Added support for custom schedules. Experimenting with the final layout for now.

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -8,10 +8,15 @@ class User(Base):
     __tablename__ = "users"
 
     phone_number = Column(Integer, primary_key=True, nullable=False)
-    channels = Column(JSON)
+    channels = Column(JSON, default=[])
+    custom_schedules = Column(JSON, default={})
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
 
     @hybrid_method
     def has_channel(self, channel):
         return channel in self.channels
+
+    @hybrid_method
+    def has_custom_schedule(self, channel):
+        return channel in self.custom_schedules

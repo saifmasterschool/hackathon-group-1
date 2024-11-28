5620ea1e07535b4aee4b93af353c8fd9fb19cd58
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-11-28 08:07:06 +0100

    [debug] removed typo

--- schemas/message_schema.py
+++ schemas/message_schema.py
@@ -6,17 +6,6 @@ from database.extension import Base
 class Message(Base):
     __tablename__ = "messages"
 
-    from sqlalchemy import Column, Integer, String, DateTime, func
-    from database.extension import Base
-    class Message(Base): __tablename__ = "messages"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    sender = Column(Integer, nullable=False)
-    message = Column(String, nullable=False)
-    receivedAt = Column(DateTime, nullable=False)
-    created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
-
     def __repr__(self):
         return f"<Message(id={self.id}, sender={self.sender}, message='{self.message}', receivedAt='{self.receivedAt}', created_at='{self.created_at}', updated_at='{self.updated_at}')>"
 

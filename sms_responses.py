d989f0243e7a475c0ce9ef765315cf7834ea06f9
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-11-28 09:43:46 +0100

    [feat] updated sms_resonses

--- sms_responses.py
+++ sms_responses.py
@@ -15,3 +15,27 @@ BROADCAST_WATER_REMINDER_MESSAGE = """This is your drinking reminder number 3. P
 DEFAULT_MESSAGE = f"""Your message could not be recognised.
 Use keywords SUBSCRIBE, UNSUBSCRIBE, STATUS, {KEYWORD_JOIN_CHANNEL} or {KEYWORD_LEAVE_CHANNEL}.
 Or just write joke or quote to instantly receive a joke or quote ;)"""
+
+
+'''BROADCAST_WATER_REMINDER_MESSAGE = """💧 Hey, hydration hero! 💧
+It's time for a water break! Remember: your body is 70% water, not coffee or soda.
+ This is your friendly reminder: DRINK UP!💦
+ Stay hydrated. Stay alive. Conquer the day! 🌟"""
+BROADCAST_JOKE_MESSAGE = """Get 3 daily doses of hilarity to keep the stress at bay!"""
+BROADCAST_QUOTE_MESSAGE = """3 inspiring quotes a day to keep you motivated and unstoppable."""
+STATUS_MESSAGE = """📊 Here's your subscription status:
+- Subscribed to: {", ".join(USER_SUBSCRIPTIONS)}.
+- Not subscribed yet? Pick your favorite from: {", ".join(AVAILABLE_CHANNELS)}.
+Want to switch it up? Just send:
+"{KEYWORD_JOIN_CHANNEL} <service>" or "STOP <service>" to unsubscribe.
+Stay awesome and stay connected! ✨"""
+WELCOME_MESSAGE = f"""🎉 Welcome to {MASTERSCHOOL_API_TEAMNAME}! 🎉
+We've got some amazing services to brighten your day! Here's how you can join the fun:
+👉 To subscribe, just send: "{KEYWORD_JOIN_CHANNEL} <service>".
+For example: "{KEYWORD_JOIN_CHANNEL} {AVAILABLE_CHANNELS[0]}".
+Here's what we offer:
+✨ JOKE: Get 3 daily doses of hilarity to keep the stress at bay!
+✨ QUOTE: 3 inspiring quotes a day to keep you motivated and unstoppable.
+✨ WATER: A reminder to hydrate and refresh yourself.
+Don't wait—join a service now and make your day awesome! 😊
+"""'''
